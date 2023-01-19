3b67c58867c6f95b3aac5512736fe9079db9b02d
Hasan Özdemir <hasanozdemir1@trakya.edu.tr>
2023-01-19 17:20:15 +0300

    result serialization

---
+++ app/encoder.py
@@ -0,0 +1,14 @@
+import json
+
+
+def serialize_query(cursor, result):
+    if result is None:
+        return None
+    else:
+        # Get column names from cursor description
+        column_names = [desc[0] for desc in cursor.description]
+        # Create a dictionary using column names as keys and values from result
+        data = dict(zip(column_names, result))
+        # Serialize the dictionary to a JSON string
+        json_data = json.dumps(data)
+        return json_data
